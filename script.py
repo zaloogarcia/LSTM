62cc9e2154da7986c147ac495ed70b90075a45fa
Gonzalo <gmg@Gnzalos-MBP.home>
2019-05-07 09:36:10 -0400

    service running and predicting(bad)

--- script.py
+++ script.py
@@ -18,39 +18,44 @@ def invert_scale(scaler, X, value):
     inverted = scaler.inverse_transform(array)
     return inverted[0, -1]
 
-def inverse_difference(history, yhat, interval=1):
-    return yhat + history[-interval]
+def inverse_difference(history, yhat):
+    return yhat + history
 
 
 def init():
     global model
-    global scaler
     model_path = Model.get_model_path(model_name='model')
     json_file = open(os.path.join(model_path, 'model.json'), 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     model = keras.models.model_from_json(loaded_model_json)
     model.load_weights(os.path.join(model_path, "model.h5"))
-    scaler = MinMaxScaler(feature_range=(-1,1))
 
 
 def run(raw_data):
     try:
         prediction = []
+        scaler = MinMaxScaler(feature_range=(-1, 1))
         raw_data = pd.DataFrame(json.loads(raw_data)['dates'])
-        days_from_init = pd.datetime(2019, 2, 2)
-        data_length = len(raw_data) + abs((days_from_init - raw_data[1]).days)
+        raw_data = pd.to_datetime(raw_data[0])
+        days_from_init = pd.to_datetime(pd.datetime(2019, 2, 2))
+        data_length = len(raw_data) + abs((days_from_init - raw_data[0]).days)
         last_prediction = [0.17562724]
-        last_value = [175.99999]
+        last_value = 175.99999
 
         for i in range(1, data_length):
-            yhat = forecast_lstm(model, 1, last_prediction)
+
+            npprediction = np.asarray(last_prediction)
+            yhat = forecast_lstm(model, 1, npprediction)
             last_prediction = [yhat]
+            npprediction = npprediction.reshape(1,-1)
+            scaler = scaler.fit(npprediction)
             yhat = invert_scale(scaler, last_prediction, yhat)
-            yhat = inverse_difference(last_value, yhat, data_length + 1 - i)
+            yhat = inverse_difference(last_value, yhat)
             last_value = yhat
             prediction.append(yhat)
         return prediction
 
     except BaseException as e:
         return str(e.args)
+
