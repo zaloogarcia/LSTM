e445ccac6059260b92b00d0823ffcdd45f48ff5c
Gonzalo <gmg@Gnzalos-MBP.home>
2019-05-09 12:34:50 -0400

    code ready for presentation

--- azuretest.py
+++ azuretest.py
@@ -1,16 +1,11 @@
 from azureml.core.workspace import Workspace
 from azureml.core.runconfig import RunConfiguration
-from azureml.core.compute import ComputeTarget, HDInsightCompute
-from azureml.exceptions import ComputeTargetException
 from azureml.core.image import ContainerImage
 from azureml.core import ScriptRunConfig, Experiment
 from azureml.core.model import Model
-from azureml.core.model import upload_dependency
 from azureml.core.conda_dependencies import CondaDependencies
 from azureml.core.webservice import AciWebservice, Webservice
 
-
-# Name of the cluster: HDInsightPI
 
 # ws = Workspace.create(name='demoGarbarino',
 #                       subscription_id='2dbdb5d8-da03-41b7-b36d-eb5574408ca6',
@@ -37,11 +32,7 @@ model = Model.register(model_path="model",
 
 
 env = CondaDependencies.create(conda_packages=['keras', 'pandas', 'scikit-learn'])
-
 env.save_to_file(base_directory='./', conda_file_path='env.yml')
-
-# with open('env.yml','w') as conda_file:
-#     conda_file.write(env.serialize_to_string())
 
 aciconfig = AciWebservice.deploy_configuration(cpu_cores=1,
                                                auth_enabled=True,
@@ -63,16 +54,4 @@ service.wait_for_deployment(show_output=True)
 print(service.get_keys())
 print(service.get_logs())
 
-# ds = ws.get_default_datastore()
-# ds.upload_files(['./train.xlsx'], target_path='train.xlsx', overwrite=True, show_progress=True)
-
-hdi_compute = ComputeTarget(ws, 'HDInsightPI')
-
-# use pyspark framework
-
-run_hdi = RunConfiguration(framework='pyspark')
-run_hdi.environment.python.user_managed_dependencies = True
-run_hdi.target = hdi_compute.name
-
-# Set compute target to the HDI cluster
 
--- train_local.py
+++ train_local.py
@@ -1,16 +1,14 @@
-from pandas import DataFrame, Series, concat, \
-    read_excel, Grouper, to_datetime, to_timedelta
+from pandas import DataFrame, Series, concat, read_excel
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 from azureml.core.run import Run
 from math import sqrt
-import matplotlib
-from matplotlib import pyplot as plt
-matplotlib.use('TkAgg') # For MACOS
 import numpy as np
-import os, pickle
+import os, matplotlib
+from matplotlib import pyplot as plt
+matplotlib.use('TkAgg')  # For MACOS
 
 
 def timeseries_to_supervised(df, lag=1):
@@ -56,23 +54,17 @@ def fit_lstm(train, batch_size, nb_epoch, neurons):
     X, y = train[:, 0:-1], train[:, -1]
     X = X.reshape(X.shape[0], 1, X.shape[1])
     model = Sequential()
-    model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1],
-                                               X.shape[2]), stateful=True))
+    model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
     model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.compile(loss='mean_squared_error',
+                  optimizer='adam')
+    # Start training
     for i in range(nb_epoch):
         if (i % 100) == 0:
             print("Epochs is at %d" % i)
         model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
         model.reset_states()
-    # weights = model.get_weights()
-    # try:
-    #     with open("model.pkl", "wb") as file_pkl:
-    #         pickle.dump(weights, file_pkl, protocol=pickle.HIGHEST_PROTOCOL)
-    # except:
-    #     with open("model.pkl", "w") as file_pkl:
-    #         pickle.dump(weights, file_pkl, protocol=pickle.HIGHEST_PROTOCOL)
-    #
+
     model_json = model.to_json()
     with open("model/model.json","w") as json_file:
         json_file.write(model_json)
@@ -81,23 +73,22 @@ def fit_lstm(train, batch_size, nb_epoch, neurons):
     return model
 
 
+# Predict from previous data
 def forecast_lstm(model, batch_size, X):
     X = X.reshape(1, 1, len(X))
     yhat = model.predict(X, batch_size=batch_size)
     return yhat[0, 0]
 
+
+# Folders that will be later uploaded
 os.makedirs('outputs', exist_ok=True)
 os.makedirs('model', exist_ok=True)
-# sc = SparkContext.getOrCreate()
-# sqlc = SQLContext(sc)
-# spark = sqlc.sparkSession
-# data_path = 'adl://storagedemo.azuredatalakestore.net/clusters/hdiclusterpi/example/data/train.csv'
-# series = spark.read.option('header', 'true').csv(data_path).toPandas()
+
+# Starts logging the variables and metrics
 run = Run.get_context()
+
+# Data pre processing
 series = read_excel('train.xlsx')
-# data = read_excel('train.xlsx')
-# data['date'] = to_datetime(data['date']) - to_timedelta(7, unit='d')
-# series = data.groupby([Grouper(key='date', freq='W-MON')])['sales'].sum().reset_index().sort_values('date')
 series.set_index('date', inplace=True)
 raw_values = series['sales'].values
 diff_values = difference(raw_values, 1)
@@ -107,18 +98,17 @@ data_length = len(series)
 div = int(data_length * 0.3)
 train, test = supervised_values[0:-div], supervised_values[-div:]
 
+# Modeling
 scaler, train_scaled, test_scaled = scale(train, test)
-print(scaler)
 neurons = 6
 batches = 2000
-run.log('neurons', neurons)
-run.log('batches', batches)
+run.log('neurons', neurons), run.log('batches', batches)
 lstm_model = fit_lstm(train_scaled, 1, batches, neurons)
 train_reshaped = train_scaled[:, 0].reshape(len(train_scaled), 1, 1)
 lstm_model.predict(train_reshaped, batch_size=1)
 
+# Model validation
 predicts = list()
-
 for i in range(len(test_scaled)):
     X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
     print(X)
@@ -132,14 +122,14 @@ for i in range(len(test_scaled)):
     run.log('True Value', expected)
     print('Day=%d, Predicted=%f, Expected=%f' % (i + 1, yhat, expected))
 
+# Check the actual error
 rmse = sqrt(mean_squared_error(raw_values[-div:], predicts))
 run.log('Mean squared error', rmse)
 print('Test RMSE: %.3f' % rmse)
 
-# print("Going to plot")
-# plt.title('LSTM with ' + str(neurons) + 'Neurons')
-# plt.plot(raw_values[-div:], label='True')
-# plt.plot(predicts, '-r', label='Prediction')
-# plt.legend(loc='best')
-#
-# run.log_image('Prediction', plot=plt)
+# Log the plot
+plt.title('LSTM with ' + str(neurons) + 'Neurons')
+plt.plot(raw_values[-div:], label='True')
+plt.plot(predicts, '-r', label='Prediction')
+plt.legend(loc='best')
+run.log_image('Prediction', plot=plt)
